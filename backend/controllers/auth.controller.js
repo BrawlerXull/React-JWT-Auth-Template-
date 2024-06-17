e85484c64123e2f2bc1b4fb837823bc8e551703f
Chinmay <chaudhari.chinmay12345@gmail.com>
2024-06-17 15:34:35 +0530

    Added error handling:

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,37 +1,33 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
-dotenv.config();
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
 
-mongoose
-  .connect(process.env.MONGO)
-  .then(() => {
-    console.log('MongoDb is connected');
-  })
-  .catch((err) => {
-    console.log(err);
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === '' ||
+    email === '' ||
+    password === ''
+  ) {
+    next(errorHandler(400, 'All fields are required'));
+  }
+
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
   });
 
-const app = express();
-
-app.use(express.json());
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
-});
-
-app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
-
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
-  res.status(statusCode).json({
-    success: false,
-    statusCode,
-    message,
-  });
-});
+  try {
+    await newUser.save();
+    res.json('Signup successful');
+  } catch (error) {
+    next(error);
+  }
+};
