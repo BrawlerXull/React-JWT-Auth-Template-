96c65e1a537cc74a6388ebea73248e106c18d152
Chinmay <chaudhari.chinmay12345@gmail.com>
2024-06-18 23:16:15 +0530

    Private route added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import { Toaster } from "sonner";
 import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
@@ -13,6 +15,9 @@ export default function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/dashboard' element={<Dashboard />} />
+        </Route>
       </Routes>
       
     </BrowserRouter>
