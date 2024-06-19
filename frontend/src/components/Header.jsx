6ddd09c1afaf2c588e1f83213a9f233c97c365c5
Chinmay <chaudhari.chinmay12345@gmail.com>
2024-06-20 01:00:50 +0530

    removed profile from header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -163,18 +163,6 @@ export default function Header() {
                   {item.name}
                 </Link>
               ))}
-              {currentUser && (
-                <Link
-                  to={"/profile"}
-                  className={classNames(
-                    "text-gray-300 hover:bg-gray-700 hover:text-white",
-                    "block rounded-md px-3 py-2 text-base font-medium"
-                  )}
-                  aria-current={false}
-                >
-                  Profile
-                </Link>
-              )}
             </div>
           </DisclosurePanel>
         </>
